f7543bc3676fac795c6a8a387c9695ae4d89aba4
suprakash <suprakashgorai14@gmail.com>
2024-01-27 19:02:06 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    firebase_apiKey: "AIzaSyC5aaZyyUu7sJEm4h7WRNDT0W56H2cPzXI",
-    firebase_authDomain: "myproject-a9ae7.firebaseapp.com",
-    firebase_projectId: "myproject-a9ae7",
-    firebase_storageBucket: "myproject-a9ae7.appspot.com",
-    firebase_messagingSenderId: "121628896966",
-    firebase_appId: "1:121628896966:web:34349769d2586bf7c7f1c7",
-    firebase_measurementId: "G-8MFGVBW30N",
-    Rapid_Api_Url: "https://jsearch.p.rapidapi.com/",
-    Rapid_Api_Key: "",
-    Rapid_Api_Host: "jsearch.p.rapidapi.com",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
